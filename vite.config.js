44c2ff8928f25cbeda2873553885fb5ccb164768
smkun <skunian@yahoo.com>
2024-05-16 12:18:50 -0400

    fixe vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3002, // Specify the port you want to use
-  },
-})
-
+    plugins: [react()],
+    server: {
+        port: 3002,
+        host: "0.0.0.0",
+    },
+});
