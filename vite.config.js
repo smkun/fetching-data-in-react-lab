97f1b9e648b796b7100187b0b75e3969e4bd042c
smkun <skunian@yahoo.com>
2024-05-16 11:50:27 -0400

    port change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3002, // Specify the port you want to use
+  },
 })
+
